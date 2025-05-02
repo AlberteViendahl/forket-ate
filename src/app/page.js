7b6afbf907ded37ff4d9b97e21eea8ab1d877366
Catharina <catharina2002@gmail.com>
2025-05-02 22:34:21 +0200

    carrousel

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import CarrouselCard from "@/components/CarrouselCard";
 import Footer from "@/components/Footer";
 import Nav from "@/components/Nav";
 
@@ -5,6 +6,15 @@ export default function Loading() {
   return (
     <div className="overflow-hidden">
       <Nav />
+      <section className="relative w-full">
+  <img src="/img/video_desk.svg" alt="video" className="w-full h-auto" />
+  <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-darkblue text-5xl font-bold">
+    ANKJÆR TOTAL ENTREPRISE
+  </h1>
+</section>
+      <section className="bg-brown">
+      <CarrouselCard/>
+      </section>
       <Footer />
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,7 +21,7 @@ function Navigation() {
 
   return (
     <header className="relative">
-      <nav className="flex justify-between items-center p-4">
+      <nav className="flex justify-between items-center p-4 bg-lightblue text-brown">
         {/* Logo */}
         <Link href="/">
           <img src="/img/logo.svg" alt="logo ikon" />
@@ -60,7 +60,6 @@ function Navigation() {
           )}
         </button>
 
-        {/* Menuen - samme UL til både mobil og desktop */}
         <ul
           className={`
             fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out flex-col gap-6 p-8
