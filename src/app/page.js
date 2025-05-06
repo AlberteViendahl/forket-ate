db13f10776306cb9de697f3b656f3554584e48e6
Sophie <saclassichouse@gmail.com>
2025-05-06 15:22:40 +0200

    knap_igen

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,14 @@
 import CarrouselCard from "@/components/CarrouselCard";
 import Footer from "@/components/Footer";
-import Knap from "@/components/Knap";
-import KontaktOs from "@/components/KontaktOs";
 import Nav from "@/components/Nav";
+import Knap from "@/components/Knap";
 import OmOs from "@/components/OmOs";
 
 export default function Loading() {
   return (
     <div className="overflow-hidden">
       <Nav />
-      <Knap />
-      <KontaktOs cla />
+
       <section className="relative w-full">
         <img src="/img/video_desk.svg" alt="video" className="w-full h-auto" />
         <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-darkblue text-5xl font-bold">
@@ -20,6 +18,7 @@ export default function Loading() {
       <section className="bg-brown">
         <OmOs />
         <CarrouselCard />
+        <Knap />
       </section>
       <Footer />
     </div>
