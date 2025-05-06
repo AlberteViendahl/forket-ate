0e6a83f52dde6c6b50edaeb62050051ca4159ce5
Catharina <catharina2002@gmail.com>
2025-05-06 11:54:24 +0200

    galleri grid

--- src/app/Inspiration/page.js
+++ src/app/Inspiration/page.js
@@ -1,9 +1,13 @@
+import Footer from "@/components/Footer";
+import Galleri from "@/components/Galleri";
 import Nav from "@/components/Nav";
 
 export default function Inspiration() {
   return (
     <div className="overflow-hidden">
       <Nav />
+      <Galleri/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Galleri.jsx
@@ -0,0 +1,46 @@
+const Galleri = () => {
+  return (
+    <section className="bg-brown p-20">
+      <h1 className="text-center text-4xl p-2 text-white">INSPIRATION</h1>
+      <p className="text-white">
+        Drømmer du om dit perfekte hjem? På denne sige kan du udforske
+        forskellige stilarter og materialer som andre drømte om - uanset om du
+        foretrækker moderne minimalisme eller klassisk hygge, er mulighederne
+        mange.​
+      </p>
+      <p className="text-white">
+        Dette gør det nemt for dig at finde præcis de elementer, der passer til
+        din stil og dine behov.​ Det betyder meget for os, at du får den
+        perfekte blanding mellem kvalitet og sætter dit eget præg.{" "}
+      </p>
+      <p className="text-white">
+        Ved at holde musen over de interaktive prikker på billederne, kan du se
+        detaljer om de anvendte produkter og materialer.{" "}
+      </p>
+      <div className="relative inline-block group">
+  <img
+    src="/img/dot.svg"
+    alt="dot"
+    className="transition-transform duration-300 transform hover:scale-110 cursor-pointer"
+  />
+  <div className="absolute left-full top-1/2 -translate-y-1/2 ml-2 w-48 p-2 bg-white text-black text-sm rounded shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none">
+    Her er information om produkter.
+  </div>
+</div>
+
+<div className="grid grid-cols-6 gap-5 p-4">
+        <img src="/img/1.svg" className="col-span-4 m-0 p-0 h-full object-cover" />
+        <img src="/img/2.svg" className="col-span-2 m-0 p-0 h-full object-cover rounded-12" />
+        <img src="/img/3.svg" className="col-span-3 m-0 p-0 h-full object-cover rounded-12" />
+        <img src="/img/4.svg" className="col-span-3 m-0 p-0 h-full object-cover rounded-12" />
+        <img src="/img/5.svg" className="col-span-2 m-0 p-0 h-full w-full object-cover rounded-12" />
+        <img src="/img/8.svg" className="col-span-4 row-span-2 m-0 p-0 h-full w-full object-cover rounded-12" />
+        <img src="/img/6.svg" className="col-span-1 m-0 p-0 h-full object-cover rounded-12" />
+        <img src="/img/7.svg" className="col-span-1 m-0 p-0 h-full object-cover rounded-12" />
+      </div>
+
+    </section>
+  );
+};
+
+export default Galleri;
