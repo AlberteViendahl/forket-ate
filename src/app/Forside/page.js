e01093eca11a9fe2c57f355089325ddc5be82cfd
Alberte <alan0002@stud.kea.dk>
2025-05-07 10:30:09 +0200

    noget af loadingpage + fikset supabase

--- src/app/Forside/page.js
+++ src/app/Forside/page.js
@@ -1,10 +1,22 @@
+import CarrouselCard from "@/components/CarrouselCard";
 import Footer from "@/components/Footer";
+import KontaktOs from "@/components/KontaktOs";
 import Nav from "@/components/Nav";
 
 export default function Forside() {
   return (
-    <div className="overflow-hidden">
+    <div /* className="overflow-hidden" */>
       <Nav />
+      <KontaktOs />
+      <section className="relative w-full">
+        <img src="/img/video_desk.svg" alt="video" className="w-full h-auto" />
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-darkblue text-5xl font-bold">
+          ANKJÆR TOTAL ENTREPRISE
+        </h1>
+      </section>
+      <section className="bg-brown">
+        <CarrouselCard />
+      </section>
       <Footer />
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,18 @@
-import CarrouselCard from "@/components/CarrouselCard";
-import Footer from "@/components/Footer";
-import Knap from "@/components/Knap";
-import KontaktOs from "@/components/KontaktOs";
-import Nav from "@/components/Nav";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import LoadingPage from "@/components/LoadingPage";
 
-export default function Loading() {
-  return (
-    <div className="overflow-hidden">
-      <Nav />
-      <Knap />
-      <KontaktOs cla />
-      <section className="relative w-full">
-        <img src="/img/video_desk.svg" alt="video" className="w-full h-auto" />
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-darkblue text-5xl font-bold">
-          ANKJÆR TOTAL ENTREPRISE
-        </h1>
-      </section>
-      <section className="bg-brown">
-        <CarrouselCard />
-      </section>
-      <Footer />
-    </div>
-  );
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/Forside");
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return <LoadingPage />;
 }
