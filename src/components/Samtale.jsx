ff2ddaddab7fb03ba160c9ab45aeccda413afed7
Alberte <alan0002@stud.kea.dk>
2025-05-22 14:18:39 +0200

    appen

--- src/components/Samtale.jsx
+++ src/components/Samtale.jsx
@@ -2,16 +2,27 @@ import Link from "next/link";
 const Samtale = () => {
   return (
     <section className="bg-beige">
-      <h2 className="text-center">Dit hus i billeder</h2>
+      <h2 className="text-center text-2xl p-5">Dit hus i billeder</h2>
       <div className="flex justify-center">
-        <p className="w-300 mb-10">
+        <p className="w-300 mb-5">
           Se de nyeste billeder fra byggepladsen og følg med i dit hjems
           udvikling – ét skridt ad gangen.
         </p>
       </div>
-
+      <img
+        className=" mx-auto mb-5"
+        src="/img/husapp.jpg"
+        alt="hus der er i gang med at blive bygget"
+      />
       <div className="flex justify-center mb-7">
-        <img className="h-75" src="/img/martin.svg" alt="" />
+        <div className="grid grid-cols-1">
+          <img
+            className="h-75 rounded-100"
+            src="/img/martin.svg"
+            alt="Billede af ejeren"
+          />
+        </div>
+
         <p className="w-300 ml-10 bg-lightblue rounded-12 p-2">
           Kære Camilla og Michael <br /> <br /> Så nåede jeres hus endnu en
           milepæl og har fået tag på!Hvor har jeg glædet mig til at dele det med
@@ -24,14 +35,20 @@ const Samtale = () => {
           Hvor ser det skønt ud, Martin! Jeg kan næsten ikke vente.. det må
           betyde, at jeg kommer forbi med kage i løbet af ugen
         </p>
-        <img className="ml-5" src="/img/person.svg" alt="" />
+        <img
+          className="ml-10"
+          src="/img/person.svg"
+          alt="illustration af en person"
+        />
       </div>
+
       <div className="flex justify-center mt-5 pb-10">
         <input
-          className="w-350 h-10 rounded-15 "
+          className="w-320 h-10 rounded-15 "
           type="text"
           placeholder=" Skriv her"
         />
+        <img className="h-30 pl-3" src="/img/send.svg" alt="send" />
       </div>
       <div className="bg-white flex justify-center p-5">
         <Link href="/App">
