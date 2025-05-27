2218054657e64a5325e847e96057798cc1d54c9c
Alberte <alan0002@stud.kea.dk>
2025-05-27 23:49:12 +0200

    kontakt

--- src/components/Formel.jsx
+++ src/components/Formel.jsx
@@ -39,7 +39,7 @@ const Formel = () => {
   };
 
   return (
-    <div className="bg-beige bg-image pb-20 px-20 pt-20 ">
+    <div className="bg-beige bg-image md:pb-20 pd:x-20 md:pt-20 xs:p-10 ">
       <section className=" bg-white rounded-15 shadow pt-5">
         <h1 className="text-brown font-extrabold text-center xs:text-2xl md:text-5xl pb-10 pt-5">
           BOOK ET MØDE
