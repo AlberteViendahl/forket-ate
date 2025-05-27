317ec9fbee601bec6be50c449369ad422b0d2c8a
Alberte <alan0002@stud.kea.dk>
2025-05-27 20:17:24 +0200

    baggrund på kontakt

--- src/components/KontaktOs.jsx
+++ src/components/KontaktOs.jsx
@@ -17,7 +17,7 @@ const KontaktOs = () => {
 
       {isOpen && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
-          <div className="relative bg-orange p-6 rounded-xl w-full m-10">
+          <div className="relative bg-lightblue p-6 rounded-xl w-full m-10">
             <button
               onClick={() => setIsOpen(false)}
               className="absolute top-2 right-2 text-2xl text-black hover:text-lightblue"
